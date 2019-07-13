24fb0c6057ccccf10b7fe05a81879dd7c3a3ac16
Jack Jiang <jackjyq@outlook.com>
2019-07-13 22:48:31 +1000

    Updates

---
+++ precache-manifest.0109e2bc3fd3d9916e76e630d38d2912.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5001473d4a4215f793d1dca59aff4402",
+    "url": "/creatAbility/index.html"
+  },
+  {
+    "revision": "745a21d2d3a819be1c68",
+    "url": "/creatAbility/static/css/main.c9efc69b.chunk.css"
+  },
+  {
+    "revision": "5ed109a51d8154465cdc",
+    "url": "/creatAbility/static/js/2.4540070d.chunk.js"
+  },
+  {
+    "revision": "745a21d2d3a819be1c68",
+    "url": "/creatAbility/static/js/main.53acd23d.chunk.js"
+  },
+  {
+    "revision": "aa5c5030f8cd80cd4aa9",
+    "url": "/creatAbility/static/js/runtime~main.5ee79f6c.js"
+  },
+  {
+    "revision": "551f500da1c9515b1c1c5e69ca1c4cc4",
+    "url": "/creatAbility/static/media/ada.551f500d.jpg"
+  },
+  {
+    "revision": "46b372a799b4ebb9bef1ce1a62879ddb",
+    "url": "/creatAbility/static/media/biglogo.46b372a7.png"
+  },
+  {
+    "revision": "3e4c815f5e92dbf93041d30850cbf1d0",
+    "url": "/creatAbility/static/media/donate.3e4c815f.png"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/creatAbility/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/creatAbility/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "43dd2bd463519478433445f8b9e6c6be",
+    "url": "/creatAbility/static/media/face.43dd2bd4.jpg"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/creatAbility/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "7bf173c79e4ed19208c053f440798437",
+    "url": "/creatAbility/static/media/good.7bf173c7.jpg"
+  },
+  {
+    "revision": "a2fbce8aec709eb57445f035ece4bb5c",
+    "url": "/creatAbility/static/media/heart.a2fbce8a.jpg"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/creatAbility/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "7f983717b90a0181c0c44bd006384e03",
+    "url": "/creatAbility/static/media/jam.7f983717.jpg"
+  },
+  {
+    "revision": "103e38baec603ba45e4608e3dba3dc5d",
+    "url": "/creatAbility/static/media/jen.103e38ba.jpg"
+  },
+  {
+    "revision": "5d3ce90d90568bc88a55e302db2910ba",
+    "url": "/creatAbility/static/media/jenny.5d3ce90d.jpg"
+  },
+  {
+    "revision": "eb35fb35e42a99b09e49db82bfe6dc48",
+    "url": "/creatAbility/static/media/jeremy.eb35fb35.jpeg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/creatAbility/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/creatAbility/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/creatAbility/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/creatAbility/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/creatAbility/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/creatAbility/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/creatAbility/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "4e4d63db98b105534e4e0619153fa1ba",
+    "url": "/creatAbility/static/media/preview.4e4d63db.jpg"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/creatAbility/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/creatAbility/static/media/ryan.f217f346.jpg"
+  },
+  {
+    "revision": "c68bb0835e2f963fb561689a88d93bfa",
+    "url": "/creatAbility/static/media/singer.c68bb083.jpg"
+  },
+  {
+    "revision": "7f7b9b2957b47e62a8a80f4c54ce2a67",
+    "url": "/creatAbility/static/media/ski.7f7b9b29.jpg"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/creatAbility/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "7f3da9211d8bdb65ae21a25eebfd610e",
+    "url": "/creatAbility/static/media/staff.7f3da921.jpg"
+  },
+  {
+    "revision": "55239cdb6a84d3aa807a0d58a2288a4c",
+    "url": "/creatAbility/static/media/toolbox.55239cdb.png"
+  }
+]);
